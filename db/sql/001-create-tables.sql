294e76a3137b81f2d977b18864b4f205ae14a59e
Tsukasa Murata <stepfootprint@gmail.com>
2020-11-15 22:41:51 +0900

    ガチャテーブル修正

--- db/sql/001-create-tables.sql
+++ db/sql/001-create-tables.sql
@@ -1,8 +1,7 @@
 ---- drop ----
 DROP TABLE IF EXISTS `chara_user`;
-DROP TABLE IF EXISTS `gacha`;
-DROP TABLE IF EXISTS `gacha_type_rate_type`;
-DROP TABLE IF EXISTS `rate_type`;
+DROP TABLE IF EXISTS `gacha_probability_group`;
+DROP TABLE IF EXISTS `gacha_probability`;
 DROP TABLE IF EXISTS `gacha_type`;
 DROP TABLE IF EXISTS `user`;
 DROP TABLE IF EXISTS `chara`;
@@ -42,53 +41,42 @@ create table IF not exists `chara_user`
 ALTER TABLE chara_user add FOREIGN KEY (user_id) references user(id);
 ALTER TABLE chara_user add FOREIGN KEY (chara_id) references chara(id);
 
-create table IF not exists `gacha_type`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `name`             VARCHAR(20) NOT NULL,
- `created_at`       Datetime NOT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャタイプ';
+-- ガチャ種類
+CREATE TABLE `gacha_type` (
+  `id`              INT(20) AUTO_INCREMENT,
+  `name`            VARCHAR(255),
+  `created_at`      Datetime NOT NULL,
+  `updated_at`      Datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャ種類';
 
-create table IF not exists `rate_type`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `name`             VARCHAR(20) NOT NULL,
- `rate`             INT(20) NOT NULL comment 'タイプごとの排出率(重み)',
- `created_at`       Datetime NOT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャ排出率タイプ';
+-- gacha_probability_group: gacha_probability_group_id=normal: 1
+-- gacha_probability_group: gacha_probability_group_id=rare: 1
+-- gacha_probability_group: gacha_probability_group_id=s_rare: 1
+-- gacha_probability_group: gacha_probability_group_id=ss_rare: 1
+-- レアリティの排出確率
+CREATE TABLE `gacha_probability_group` (
+  `gacha_type_id`              INT(20),
+  `gacha_probability_group_id` VARCHAR(255),
+  `rate`                       INT(20) NOT NULL DEFAULT 1,
+  `created_at`                 Datetime NOT NULL,
+  `updated_at`                 Datetime DEFAULT NULL,
+  PRIMARY KEY (`gacha_type_id`, `gacha_probability_group_id`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャ確率グループ';
 
-create table IF not exists `gacha_type_rate_type`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `gacha_type_id`    INT(20) NOT NULL,
- `rate_type_id`     INT(20) NOT NULL,
- `created_at`       Datetime NOT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャタイプで使用する排出率タイプ';
+-- gacha_probability: chara_id=hoge: 1
+-- gacha_probability: chara_id=fuga: 1
+-- レアリティごとの排出内容
+CREATE TABLE `gacha_probability` (
+  `group_id`    VARCHAR(255), -- normal
+  `number`      INT(20), -- 1,2,3
+  `chara_id`    INT(20) NOT NULL,
+  `rate`        INT(20) NOT NULL DEFAULT 1,
+  `created_at`  Datetime NOT NULL,
+  `updated_at`  Datetime DEFAULT NULL,
+  PRIMARY KEY (`group_id`, `number`)
+) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャ確率';
 
-create UNIQUE INDEX gacha_type_rate_type_index on gacha_type_rate_type(gacha_type_id, rate_type_id);
-ALTER TABLE gacha_type_rate_type add FOREIGN KEY (gacha_type_id) references gacha_type(id);
-ALTER TABLE gacha_type_rate_type add FOREIGN KEY (rate_type_id) references rate_type(id);
-
-create table IF not exists `gacha`
-(
- `id`               INT(20) AUTO_INCREMENT,
- `chara_id`         INT(20) NOT NULL,
- `gacha_type_id`    INT(20) NOT NULL,
- `rate_type_id`     INT(20) NOT NULL,
- `rate`             INT(20) NOT NULL DEFAULT 1,
- `created_at`       Datetime NOT NULL,
- `updated_at`       Datetime DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci comment='ガチャ排出キャラ';
-
-create UNIQUE INDEX gacha_index on gacha(chara_id, gacha_type_id);
-ALTER TABLE gacha add FOREIGN KEY (chara_id) references chara(id);
-ALTER TABLE gacha add FOREIGN KEY (gacha_type_id) references gacha_type(id);
-ALTER TABLE gacha add FOREIGN KEY (rate_type_id) references rate_type(id);
-ALTER TABLE gacha add FOREIGN KEY (gacha_type_id, rate_type_id) references gacha_type_rate_type(gacha_type_id, rate_type_id);
+ALTER TABLE gacha_probability_group add FOREIGN KEY (gacha_probability_group_id) references gacha_probability(group_id);
+ALTER TABLE gacha_probability_group add FOREIGN KEY (gacha_type_id) references gacha_type(id);
+ALTER TABLE gacha_probability add FOREIGN KEY (chara_id) references chara(id);
--- db/sql/002-insert-initial-data.sql
+++ db/sql/002-insert-initial-data.sql
@@ -1,46 +1,49 @@
 ---- user ----
 INSERT INTO user (name, token, created_at) VALUES ("testuser1", "testtoken1", NOW());
 INSERT INTO user (name, token, created_at) VALUES ("testuser2", "testtoken2", NOW());
+INSERT INTO user (name, token, created_at) VALUES ("testuser3", "testtoken3", NOW());
+INSERT INTO user (name, token, created_at) VALUES ("testuser4", "testtoken4", NOW());
+INSERT INTO user (name, token, created_at) VALUES ("testuser5", "testtoken5", NOW());
 
 ---- chara ----
 INSERT INTO chara (name, created_at) VALUES ("testchara1", NOW());
 INSERT INTO chara (name, created_at) VALUES ("testchara2", NOW());
 INSERT INTO chara (name, created_at) VALUES ("testchara3", NOW());
 INSERT INTO chara (name, created_at) VALUES ("testchara4", NOW());
-
+INSERT INTO chara (name, created_at) VALUES ("testchara5", NOW());
+INSERT INTO chara (name, created_at) VALUES ("testchara6", NOW());
+INSERT INTO chara (name, created_at) VALUES ("testchara7", NOW());
+INSERT INTO chara (name, created_at) VALUES ("testchara8", NOW());
+INSERT INTO chara (name, created_at) VALUES ("testchara9", NOW());
+INSERT INTO chara (name, created_at) VALUES ("testchara10", NOW());
 
 ---- gacha_type ----
-INSERT INTO gacha_type (name, created_at) VALUES ("プラチナ", NOW());
-INSERT INTO gacha_type (name, created_at) VALUES ("ピックアップ", NOW());
+INSERT INTO gacha_type (name, created_at) VALUES ("platinum", NOW());
+INSERT INTO gacha_type (name, created_at) VALUES ("pickup", NOW());
 
----- rate_type ----
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★3",  25, NOW());
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★2", 180, NOW());
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★1", 795, NOW());
+---- gacha_probability ----
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★1", 1, 1, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★1", 2, 2, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★1", 3, 3, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★1", 4, 4, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★1", 5, 5, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★2", 1, 6, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★2", 2, 7, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★2", 3, 8, NOW());
 
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★3",  18, NOW());
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★3PU", 7, NOW());
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★2", 180, NOW());
-INSERT INTO rate_type (name, rate, created_at) VALUES ("★1", 795, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★3", 1, 9, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★3", 2, 10, NOW());
 
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★3PU", 1, 9, NOW());
+INSERT INTO gacha_probability (group_id, number, chara_id, created_at) VALUES ("★3NOPU", 1, 10, NOW());
 
----- gacha_type_rate_type ----
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (1, 1, NOW());
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (1, 2, NOW());
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (1, 3, NOW());
+---- gacha_probability_group ----
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (1, "★1", 795, NOW());
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (1, "★2", 180, NOW());
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (1, "★3", 25, NOW());
 
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (2, 4, NOW());
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (2, 5, NOW());
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (2, 6, NOW());
-INSERT INTO gacha_type_rate_type (gacha_type_id, rate_type_id, created_at) VALUES (2, 7, NOW());
-
----- gacha ----
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (1, 1, 1, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (2, 1, 2, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (3, 1, 3, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (4, 1, 3, NOW());
-
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (1, 2, 4, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (2, 2, 5, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (3, 2, 6, NOW());
-INSERT INTO gacha (chara_id, gacha_type_id, rate_type_id, created_at) VALUES (4, 2, 7, NOW());
+---- gacha_probability_group ----
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (2, "★1", 795, NOW());
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (2, "★2", 180, NOW());
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (2, "★3PU", 7, NOW());
+INSERT INTO gacha_probability_group (gacha_type_id, gacha_probability_group_id, rate, created_at) VALUES (2, "★3NOPU", 18, NOW());
